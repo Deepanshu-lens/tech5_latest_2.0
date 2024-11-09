8585fc3c282d9a7ec3b1a8323ae8f24cf4000bd0
Debayan Deb <debayan@MacBook-Pro-8.local>
2024-11-09 18:40:44 +0100

    client-side only

--- src/lib/pb.ts
+++ src/lib/pb.ts
@@ -1,5 +1,5 @@
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase("https://vms.lenscorp.cloud/api");
+const pb = new PocketBase("http://vms.lenscorp.cloud:5555");
 
 export default pb;
