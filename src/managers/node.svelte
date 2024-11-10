e7697c3198ace1c00b0b77e57cb45113c4d7d0fd
Kaustav-lens <kaustav.bhattacharya@lenscorp.ai>
2024-11-10 19:14:06 +0530

    fix: camera list scroll

--- src/managers/node.svelte
+++ src/managers/node.svelte
@@ -12,11 +12,6 @@
         filter: `session.id?="${pb.authStore.model.session[0]}"`,
         sort: "-created",
       });
-      console.log(
-        localNodes.length,
-        localNodes,
-        localStorage.getItem("selectedNode")
-      );
       nodes.set(localNodes);
       localNodes.length > 0 &&
         (localStorage.getItem("selectedNode")
