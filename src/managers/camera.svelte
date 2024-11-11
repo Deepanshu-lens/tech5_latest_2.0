edcd60b971e3bef7f566f7225c5e05c48df81e4c
Debayan Deb <debayan@MacBook-Pro-8.local>
2024-11-11 17:30:16 -0500

    node db fixed

--- src/managers/camera.svelte
+++ src/managers/camera.svelte
@@ -18,27 +18,42 @@
     }
   };
 
-  $: if ($selectedNode) getInitialCameras($selectedNode);
-
-  try {
-    pb.collection("camera").subscribe("*", (e) => {
-      console.log("Camera collection updated", e.action, e.record);
-      if (e.action === "create") {
-        const validated = validateCamera(e.record);
-        if (validated) cameras.update((current) => [...current, validated]);
-      } else if (e.action === "update") {
-        const validated = validateCamera(e.record);
-        if (validated)
-          cameras.update((current) =>
-            current.map((cam) => (cam.id === validated.id ? validated : cam))
+  $: if ($selectedNode) {
+    getInitialCameras($selectedNode);
+    pb.collection("camera").unsubscribe("*");
+    try {
+      pb.collection("camera").subscribe(
+        "*",
+        (e) => {
+          console.log(
+            "Camera collection updated",
+            e.action,
+            e.record,
+            $selectedNode
           );
-      } else if (e.action === "delete") {
-        cameras.update((current) =>
-          current.filter((cam) => cam.id !== e.record.id)
-        );
-      }
-    });
-  } catch (error) {
-    console.error("Failed realtime camera");
+          if (e.action === "create") {
+            const validated = validateCamera(e.record);
+            if (validated) cameras.update((current) => [...current, validated]);
+          } else if (e.action === "update") {
+            const validated = validateCamera(e.record);
+            if (validated)
+              cameras.update((current) =>
+                current.map((cam) =>
+                  cam.id === validated.id ? validated : cam
+                )
+              );
+          } else if (e.action === "delete") {
+            cameras.update((current) =>
+              current.filter((cam) => cam.id !== e.record.id)
+            );
+          }
+        },
+        {
+          filter: `node.id?="${$selectedNode}"`,
+        }
+      );
+    } catch (error) {
+      console.error("Failed realtime camera");
+    }
   }
 </script>
