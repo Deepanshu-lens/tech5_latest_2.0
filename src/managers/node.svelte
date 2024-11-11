9cc16bd1d0c47b531d9eb71f9f6e5d07a7509782
Debayan Deb <debayan@MacBook-Pro-8.local>
2024-11-11 14:06:27 -0500

    filter subscribed node

--- src/managers/node.svelte
+++ src/managers/node.svelte
@@ -26,20 +26,26 @@
 
   // //   initCameraManager();
   try {
-    pb.collection("node").subscribe("*", (e) => {
-      console.log("Node collection updated", e.action, e.record);
-      if (e.action === "create") {
-        nodes.update((current) => [...current, e.record]);
-      } else if (e.action === "update") {
-        nodes.update((current) =>
-          current.map((cam) => (cam.id === e.record.id ? e.record : cam))
-        );
-      } else if (e.action === "delete") {
-        nodes.update((current) =>
-          current.filter((cam) => cam.id !== e.record.id)
-        );
+    pb.collection("node").subscribe(
+      "*",
+      (e) => {
+        console.log("Node collection updated", e.action, e.record);
+        if (e.action === "create") {
+          nodes.update((current) => [...current, e.record]);
+        } else if (e.action === "update") {
+          nodes.update((current) =>
+            current.map((cam) => (cam.id === e.record.id ? e.record : cam))
+          );
+        } else if (e.action === "delete") {
+          nodes.update((current) =>
+            current.filter((cam) => cam.id !== e.record.id)
+          );
+        }
+      },
+      {
+        filter: `session.id?="${pb.authStore.model.session[0]}"`,
       }
-    });
+    );
   } catch (error) {
     console.error("Failed realtime camera");
   }
