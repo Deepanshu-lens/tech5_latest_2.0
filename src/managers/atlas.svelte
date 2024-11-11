ab2af357c31a423e28b3733a1d5a2ab673972b82
Kaustav-lens <kaustav.bhattacharya@lenscorp.ai>
2024-11-11 15:12:55 +0530

    chore: panel fetching with filter

--- src/managers/atlas.svelte
+++ src/managers/atlas.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import pb from "@/lib/pb";
-    import { activePanel, panels } from "@/stores";
+    import { activePanel, panels,user } from "@/stores";
   
     (async () => {
       try {
         // Fetch initial data
         pb.autoCancellation(false);
         const localpanels = await pb.collection("atlas").getFullList<any>({
-            expand:"users,doors"
+            expand:"users,doors",
+            filter: `session = "${$user?.session[0]}"`
         });
         panels.set(localpanels);
         localpanels.length > 0 && activePanel.set(localpanels[0].id);
